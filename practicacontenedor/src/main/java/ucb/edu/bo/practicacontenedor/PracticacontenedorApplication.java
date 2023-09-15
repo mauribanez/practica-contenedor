e5125224749cef5cd93f07a85931959bffa070b6
mauribanez <117604787+mauribanez@users.noreply.github.com>
2023-09-15 00:10:05 +0000

    Primer commit: Agregado archivo start.spring

---
+++ practicacontenedor/src/main/java/ucb/edu/bo/practicacontenedor/PracticacontenedorApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.practicacontenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticacontenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticacontenedorApplication.class, args);
+	}
+
+}
---
+++ practicacontenedor/src/test/java/ucb/edu/bo/practicacontenedor/PracticacontenedorApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.practicacontenedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticacontenedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
